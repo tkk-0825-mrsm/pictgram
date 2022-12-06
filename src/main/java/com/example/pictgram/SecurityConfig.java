0bc039349970f4d89021a783e41e6138ea8f5a63
Morishima Takako <tkkhr.0322@outlook.jp>
2022-12-06 11:18:28 +0900

    1206update

--- src/main/java/com/example/pictgram/SecurityConfig.java
+++ src/main/java/com/example/pictgram/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private FormAuthenticationProvider authenticationProvider;
 
     //private static final String[] URLS = { "/css/**", "/images/**", "/scripts/**", "/h2-console/**" };
-    private static final String[] URLS = { "/css/**", "/images/**", "/scripts/**", "/h2-console/**", "/favicon.ico" };
+    //private static final String[] URLS = { "/css/**", "/images/**", "/scripts/**", "/h2-console/**", "/favicon.ico" };
+    private static final String[] URLS = { "/css/**", "/images/**", "/scripts/**", "/h2-console/**", "/favicon.ico", "/OneSignalSDKWorker.js" };
     /**
     * 認証から除外する
     */
